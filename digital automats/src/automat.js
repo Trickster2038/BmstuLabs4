a81f84064266feaeeb57c5c104999f580f6d70ae
Sergey <33185046+Trickster2038@users.noreply.github.com>
2021-04-19 23:12:46 +0300

    digital automats dz code done

--- digital automats/src/automat.js
+++ digital automats/src/automat.js
@@ -1,28 +1,24 @@
 var q = 0
 var lamda = 0
-const crosses = [[[1,2,3],[0,0,0]],
-                [[0,0,0],[3,1,2]],
-                [[0,0,0],[2,3,1]]
-                [[0,0,0],[1,2,3]]]
+const crosses = [[[1, 2, 3], [0, 0, 0]],
+[[0, 0, 0], [3, 1, 2]],
+[[0, 0, 0], [2, 3, 1]],
+[[0, 0, 0], [1, 2, 3]]]
 const states = ["Ожидание нового кона", "Камень у И-1", "Ножницы у И-1", , "Бумага у И-1"]
-const results = ["Игра не закончена", "Победил И-1", "Победил И-2", , "Ничья"]
+const results = ["Игра не закончена", "Победил И-1", "Победил И-2", "Ничья"]
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     updateUI(q, lamda);
-  });
+});
 
-function changeState(signal, p_crosses=crosses){
-    console.log("q")
-    console.log(q)
-    console.log("s")
-    console.log(signal)
-    lamda = p_crosses[q][1][signal]
-    q = p_crosses[q][0][signal]
+function changeState(signal) {
+    lamda = crosses[q][1][signal]
+    q = crosses[q][0][signal]
     updateUI(q, lamda)
 }
-function updateUI(p_q, p_lamda){
+function updateUI(p_q, p_lamda) {
     turnInput = document.getElementById("nameInput")
-    if (p_q == 0){
+    if (p_q == 0) {
         turnInput.value = "Игрок 1"
     } else {
         turnInput.value = "Игрок 2"
