4bd5b8ecd919e6f49c5c76358bb9925ba3ec79bf
Sergey <33185046+Trickster2038@users.noreply.github.com>
2021-05-06 17:32:02 +0300

    trps lab3 done

--- trps/lab3/src/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ trps/lab3/src/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -17,54 +17,71 @@ using namespace std::chrono;
 
 int main()
 {
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
-    PROCESS_MEMORY_COUNTERS memCounter1;
-    BOOL result = K32GetProcessMemoryInfo(GetCurrentProcess(), &memCounter1, sizeof(memCounter1));
+	PROCESS_MEMORY_COUNTERS memCounter1;
+	BOOL result = K32GetProcessMemoryInfo(GetCurrentProcess(), &memCounter1, sizeof(memCounter1));
 
-    for (int cnt = 0; cnt < 1; cnt++) { //100k by default
-        int N = 10, i, j, k = 0, f = 0;
-        double* a;
-        double pr = 1;
-        a = (double*)malloc(N * sizeof(double));
-        srand(time(NULL));
+	for (int cnt = 0; cnt < 1; cnt++) { //100k by default
+		int N, halfRange, i, j, k = 0, f = 0;
+		int inputN, inputHr;
+		double* a;
+		double pr = 1;
+		srand(time(NULL));
 
-        a[0] = -100 + rand() % (100 + 100 + 1);
-        for (i = 1; i < N; i++) {
-            while (f == 0) {
-                a[i] = -100 + rand() % (100 + 100 + 1);
-                for (j = 0; j < i; j++) {
-                    if (a[i] != a[j]) k++;
-                }
-                if (k == i) f = 1;
-                k = 0;
-            }
-            f = 0;
+		printf("\n%s\n", "Input length of array (<100)");
+		inputN = scanf_s("%d", &N);
 
-        }
-        //printf("Default array:\n");
-        for (i = 0; i < N; i++) {
-            //printf("%f\n ", a[i]);
-        }
-        for (i = 0; i < N; i++) {
-            pr *= a[i];
-        }
-        //printf("Product:%f\n", pr);
-    }
+		if (inputN == 1 && N > 0 && N < 100) {
+			a = (double*)malloc(N * sizeof(double));
 
-    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    duration<double> time_span = t2 - t1;
-    std::cout << "It took me " << time_span.count() << " seconds.";
-    std::cout << std::endl;
+			printf("\n%s\n", "Input half-range of number >=N");
+			inputHr = scanf_s("%d", &halfRange);
+			if (inputHr == 1 && halfRange > 0 && halfRange >= N) {
+				
+				for (i = 0; i < N; i++) {
+					while (f == 0) {
+						f = 1;
+						a[i] = -halfRange + rand() % (2 * halfRange + 1);
+						for (j = 0; j < i; j++) {
+							if (a[i] == a[j]) f = 0;
+						}
+					}
+					f = 0;
+				}
+				printf("\nDefault array:\n");
+				for (i = 0; i < N; i++) {
+					printf("%.2f\n ", a[i]);
+				}
+				for (i = 0; i < N; i++) {
+					pr *= a[i];
+				}
+				printf("\nProduct:%.2f\n", pr);
+			}
+			else {
+				printf("\n%s\n", "Invalid half-range of element");
+			}
 
-    PROCESS_MEMORY_COUNTERS memCounter2;
-    result = K32GetProcessMemoryInfo(GetCurrentProcess(), &memCounter2, sizeof(memCounter2));
-    std::cout << "Memory leak: " << memCounter2.WorkingSetSize - memCounter1.WorkingSetSize << std::endl;
+			free(a);
+		}
+		else {
+			printf("\n%s\n", "Invalid length of array");
+		}
+	}
 
-    std::cout << "\n(press any key to exit)\n";
-    std::getchar();
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	duration<double> time_span = t2 - t1;
+	std::cout << "It took me " << time_span.count() << " seconds.";
+	std::cout << std::endl;
 
-    return 0;
+	PROCESS_MEMORY_COUNTERS memCounter2;
+	result = K32GetProcessMemoryInfo(GetCurrentProcess(), &memCounter2, sizeof(memCounter2));
+	std::cout << "Memory leak: " << memCounter2.WorkingSetSize - memCounter1.WorkingSetSize << std::endl;
+
+	std::cout << "\n(press any key to exit)\n";
+	std::getchar();
+
+	return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
