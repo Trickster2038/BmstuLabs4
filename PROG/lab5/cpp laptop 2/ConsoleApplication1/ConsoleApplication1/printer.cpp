497642c796434ea12aa3461b44c9e48c06e77052
LaptopSSDacc <fzastahov@gmail.com>
2021-05-17 00:51:46 +0300

    mzia build 2

---
+++ PROG/lab5/cpp laptop 2/ConsoleApplication1/ConsoleApplication1/printer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "printer.h"
+#include <string.h>
+
+extern void __stdcall PRINT(char* str) {
+	int lg;
+	lg = strlen(str);
+	if (lg > 0) {
+		printf("\nResult: %s", str);
+	}
+	else {
+		std::cout << "\nNo repeats found\n";
+	}
+	char buf[255];
+	std::cin.getline(buf, 255);
+}
